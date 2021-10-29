f34a9feb3495b0d7c67a59e7b629b7332d116207
panda <zhonghaoyu555@gmail.com>
2021-10-29 17:25:40 +0800

    update

--- chapter-01/recipe-09/cxx-example/animal-farm.cpp
+++ chapter-01/recipe-09/cxx-example/animal-farm.cpp
@@ -1,4 +1,4 @@
-#include "Animal.hpp"
+﻿#include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
 #include "Factory.hpp"
@@ -12,10 +12,15 @@ typedef std::function<std::unique_ptr<Animal>(const std::string &)> CreateAnimal
 
 int main() {
   Factory<CreateAnimal> farm;
+
+  //把lambda放入factory的map里
+
   farm.subscribe("CAT",
                  [](const std::string &n) { return std::make_unique<Cat>(n); });
   farm.subscribe("DOG",
                  [](const std::string &n) { return std::make_unique<Dog>(n); });
+
+  //farm.create先从map里取出lambda，然后再用变参函数调用
 
   std::unique_ptr<Animal> simon = farm.create("CAT", "Simon");
   std::unique_ptr<Animal> marlowe = farm.create("DOG", "Marlowe");
